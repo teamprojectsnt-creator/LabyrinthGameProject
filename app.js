66e49dc8da688eb95dda67fc3f9935d39d9b32a2
Mukhammad Akbar <akbaraliy111@example.com>
2026-07-02 20:37:29 +0500

    akbarjon

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 import { start } from "./info/menu.js"
 
 const player = start()
-for(const row of player.getInfo.get("labirint").get("labirint")){
-    console.log(row.join(""))
-}
-// console.log(player.getInfo.get("labirint").get("labirint"))
 if (!player) console.log("O'yin tugadi")
