377463860d372293ec6d30a2af9f4334a4473705
Mukhammad Akbar <akbaraliy111@example.com>
2026-07-02 20:25:48 +0500

    time function

---
+++ time/time.js
@@ -0,0 +1,13 @@
+export {
+    diffTime, timeStart
+};
+
+function timeStart(){
+    const date = Date.now();
+    return date;
+};
+
+function diffTime(timeStart){
+    const date = Date.now();
+    return Math.floor((date - start) / 1000);
+};
