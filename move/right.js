a438b111c1e8dc8ac4f29a00b43881fadc30c5d7
Ayyub <ayyubxonmashrapov@gmail.com>
2026-07-02 22:02:54 +0500

    rightMove end

--- move/right.js
+++ move/right.js
@@ -1,9 +1,16 @@
-import { Player } from "../Player/Player.js";
+// import { Player } from "../Player/Player.js";
 import { Icons } from "../data/labirint.js"
-
+import { timeStart } from "../time/time.js"
 
 class RightMove {
     static rightMove(player){
+        // console.log(player.getInfo.get('startTime'))
+        if (!player.getInfo.get('startTime')){
+            player.setStartTime = timeStart()
+        }
+
+        // console.log(player.getInfo.get('startTime'))
+
         const location = player.getInfo.get('labirint').get('location');
         const labirint = player.getInfo.get('labirint').get('labirint');
 
@@ -22,29 +29,14 @@ class RightMove {
             labirint[location[0]][location[1]] = Icons.player;
             return 'continue'; 
         }
-
-
     }       
 }
 
+// const player = new Player("a", 1)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// RightMove.rightMove(player)
+// RightMove.rightMove(player)
 
         // console.log(labirint[location[0]][location[1]])////////////////////////////////////////
