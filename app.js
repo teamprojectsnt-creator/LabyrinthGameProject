a591471ff70621e1df33dd02d59e1b97518c671e
Abdujalil <abdujalil@gmail.com>
2026-07-02 19:51:49 +0500

    icons class ochildi

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 import { start } from "./info/menu.js"
 
 const player = start()
+for(const row of player.getInfo.get("labirint").get("labirint")){
+    console.log(row.join(""))
+}
+// console.log(player.getInfo.get("labirint").get("labirint"))
 if (!player) console.log("O'yin tugadi")
