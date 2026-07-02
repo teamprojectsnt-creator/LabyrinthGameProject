e9e83586435280668b493ef39c36bad95ec33492
Ayyub <ayyubxonmashrapov@gmail.com>
2026-07-02 18:38:06 +0500

    Player class

---
+++ Player/Player.js
@@ -0,0 +1,52 @@
+export class Player {
+    constructor(name , id){
+        this.name = name;
+        this.id = id;
+    }
+
+    #startTime = undefined;
+    #endTime = undefined;
+    #stars = 0;
+    #steps = [];
+    #labirints = [];
+
+    //get info
+    //all set
+
+    set setStartTime(time){ // milliSek
+        if (!time) return
+        this.#startTime = time;
+    }
+
+    set setEndTime(time){  // milliSek
+        if (!time) return
+        this.#endTime = time;
+    }
+
+    set addStars(starsCount = 0){
+        if (!starsCount) return
+        this.#stars = starsCount;
+    }
+
+    set setSteps(stepsCount){
+        if (!stepsCount) return
+        this.#steps = stepsCount;
+    }
+
+    set setLabirint(map){
+        if (!map.length) return
+        this.#labirints = map;
+    }
+
+    get getInfo(){
+        return new Map([
+            ['startTiem', this.#startTime],
+            ['endTiem', this.#endTime],
+            ['stars', this.#stars],
+            ['steps', this.#steps],
+            ['labirints', this.#labirints]
+        ])
+    }
+}
+
+
