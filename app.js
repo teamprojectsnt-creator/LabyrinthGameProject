69fb135d11d43b4209ff25ee2129ee8fba7cea63
Abdujalil <abdujalil@gmail.com>
2026-07-02 20:46:20 +0500

    app.js switch

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-import { start } from "./info/menu.js"
-
+import { start, prompt, moveInfo } from "./info/menu.js"
 const player = start()
-for(const row of player.getInfo.get("labirint").get("labirint")){
-    console.log(row.join(""))
+while (true) {
+    for(const row of player.getInfo.get("labirint").get("labirint")){
+        console.log(row.join(""))
+    }
+    console.log(moveInfo())
+    const choice = prompt("yuring: ")
+    switch (choice) {
+        case "a":
+            (player)
+            break;
+        case "w":
+            
+            break;
+        case "d":
+            
+            break;
+        case "s":
+            
+            break;
+        default:
+            console.log("xato tanlov qildingiz❌ Iltimos qaytadan kiriting❗️")
+            continue
+    }
 }
 // console.log(player.getInfo.get("labirint").get("labirint"))
 if (!player) console.log("O'yin tugadi")
