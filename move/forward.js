42e1ead6a7451db7396fe085177128b1eba36e10
Abubakir <abubakirbotirov51@gmail.com>
2026-07-03 13:55:59 +0500

    .

--- move/forward.js
+++ move/forward.js
@@ -1,1 +1,11 @@
-console.log("Moving forward...");
+import { labirint } from "../data/labirint.js";
+import { Player } from "../Player/Player.js";
+import { labirint, Icons } from "../data/labirint.js";
+
+export class ForwardMove {
+
+    forward (payer) {
+        let personaj = labirint[player.getInfo.get('labirint').get('location')[0]][player.getInfo.get('labirint').get('location')[1]];
+        return personaj;
+    }
+}
