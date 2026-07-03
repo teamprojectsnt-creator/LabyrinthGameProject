0842f00a4f8791290e2d8d99568e1326a7821e27
Abubakir <abubakirbotirov51@gmail.com>
2026-07-03 17:56:10 +0500

    a

--- move/forward.js
+++ move/forward.js
@@ -1,11 +1,33 @@
 import { labirint } from "../data/labirint.js";
 import { Player } from "../Player/Player.js";
-import { labirint, Icons } from "../data/labirint.js";
+import { Icons } from "../data/labirint.js";
 
 export class ForwardMove {
 
-    forward (payer) {
-        let personaj = labirint[player.getInfo.get('labirint').get('location')[0]][player.getInfo.get('labirint').get('location')[1]];
-        return personaj;
+    static forward (payer) {
+
+        const info = player.getInfo();
+
+        if (info.get('startTime') === undefined) {
+            player.setStartTime = Date.now();
+        }
+
+        const labirintInfo = info.get('labirint');
+        const labirint = labirintInfo.get('labirint');
+        const location = labirintInfo.get('location');
+        
+        let row = location[0];
+        let col = location[1];
+        let nextRow = row - 1;
+
+        if (labirint[nextRow][col] === Icons.wall) {
+            console.log("Game over")
+        }
+
     }
 }
+
+
+const player = new Player('A', 1);
+
+console.log(ForwardMove.forward());
