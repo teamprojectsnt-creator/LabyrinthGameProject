5ee0666d283e4c9d0804abe48325af37dff7657f
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2026-07-03 19:19:33 +0500

    left.js

--- move/left.js
+++ move/left.js
@@ -1,9 +1,31 @@
-import { timeStart, diffTime } from "../time/time.js"
+import { Icons } from "../data/labirint.js";
+import { timeStart } from "../time/time.js";
 
-class left {
-    left(player) {
-        oldPos = player.get('location');
-        console.log(oldPos);
+export class LeftMove{
+    static leftMove(player){
+
+        if (!player.getInfo.get("startTime")){
+            playersetStartTime = timeStart()
+        }
+
+        const location = player.getInfo.get('location');
+        const labirint = player.getInfo.get('labirint');
+
+        labirint[location[0]][location[1]]=Icons.road;
         
+        player.setLocation = [location[0],--location[1]]
+
+        if (labirint[location[0]][location[1]]== Icons.wall){
+            labirint[location[0]][location[1]]=Icons.death;
+            return 'death';
+
+        }else if (labirint[location[0]][location[1]]== Icons.finish){
+            labirint[location[0]][location[1]] = Icons.players;
+            return 'finish';
+        }
+        else{
+            labirint[location[0]][location[1]] = Icons.player;
+            return 'continue';
+        }
     }
 }
